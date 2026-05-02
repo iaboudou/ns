2132e93223d82ed48e899c03dbe04da4b6a74883
ahtalbi <obetox1@gmail.com>
2026-05-02 20:54:42 +0100

    delet unused func and add some thigngs

--- backend/routes/middleware.go
+++ backend/routes/middleware.go
@@ -42,19 +42,3 @@ func (h *Handler) CORSMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		next.ServeHTTP(w, r)
 	})
 }
-
-func CORSMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
--- frontend/src/lib/socketWorker.js
+++ frontend/src/lib/socketWorker.js
@@ -1,5 +1,6 @@
 let worker;
 
+// this function is to get et the socket worker when ever needed or initlize it
 export function getSocketWorker() {
     if (typeof window === "undefined") return null;
 
